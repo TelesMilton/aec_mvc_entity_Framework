c255e9cdf2a0b55eb34128b800a34cd9ba98ef22
Milton <telesmilton1000@gmail.com>
2021-08-16 21:00:25 -0300

    Subindo projeto

---
+++ Models/Aluno.cs
@@ -0,0 +1,14 @@
+namespace aec_mvc_entity_framework.Models
+{ 
+   public class Aluno
+   {
+        public int Id { get;set; }
+    
+        public string Nome { get;set; }
+    
+        public string Matricula { get;set; }
+   
+        public string Notas { get;set; }   
+
+   }
+}
